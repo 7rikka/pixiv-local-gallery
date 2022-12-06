c777874b62d7395a4b848035c28e8415cc916461
ho <takanashirikkax@gmail.com>
2022-12-06 22:46:07 +0800

    添加主程序入口

--- src/main/java/nya/nekoneko/pixiv/Main.java
+++ src/main/java/nya/nekoneko/pixiv/Main.java
@@ -1,7 +1,14 @@
 package nya.nekoneko.pixiv;
 
+import org.noear.solon.Solon;
+
+/**
+ * 主程序入口
+ *
+ * @author Ho
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Solon.start(Main.class, args);
     }
 }
